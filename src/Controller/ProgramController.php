0a739ff1a9c8bd589ca0c41deba873829d0a2fe2
Valerie <valerie.ielsch@gmail.com>
2022-11-15 11:56:39 +0100

    challenge 05 finished

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -7,12 +7,19 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 
+#[Route('/program', name: 'program_')]
 class ProgramController extends AbstractController
 {
-    #[Route('/program/', name: 'program_index')]
+    #[Route('/', name: 'index')]
     public function index(): Response
     {
-    //  return new Response('<html><body>Wild Series Index</body></html>');
-        return $this->render('program/index.html.twig', ['website'=>'Wild Series', ]);
+        //  return new Response('<html><body>Wild Series Index</body></html>');
+        return $this->render('program/index.html.twig', ['website' => 'Wild Series',]);
+    }
+
+    #[Route('/{id}', methods: ['GET'], requirements: ['id' => '\d+'], name: 'show')]
+    public function show($id): Response
+    {
+        return $this->render('program/show.html.twig', ['id' => $id]);
     }
 }
