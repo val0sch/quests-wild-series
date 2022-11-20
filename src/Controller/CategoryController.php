7d03240802d13d3a999be4eb6cebf9157fd1c2ac
Valerie <valerie.ielsch@gmail.com>
2022-11-20 23:58:34 +0100

    in progress form ok but error on /program displaying category

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -8,6 +8,8 @@ use App\Repository\ProgramRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Form\CategoryType;
+use Symfony\Component\HttpFoundation\Request;
 
 #[Route('/category', name: 'category_')]
 class CategoryController extends AbstractController
@@ -17,9 +19,44 @@ class CategoryController extends AbstractController
     {
         $categories = $categoryRepository->findAll();
 
+
         return $this->render('category/index.html.twig', [
             'categories' => $categories,
         ]);
+    }
+
+    //Attention : Comme la route est de la forme /category/new, 
+    //veille à bien la placer au dessus de la méthode show(). 
+    //En effet, si tu la places après, le routeur de Symfony s'arrêtant à
+    // la première route qui "match", il va penser que tu veux afficher la catégorie
+    // qui a pour name "new" et tu auras une erreur 404.
+    #[Route('/new', name: 'new')]
+    public function new(Request $request, CategoryRepository $categoryRepository): Response
+    {
+        $category = new Category();
+
+        // Create the form, linked with $category
+        $form = $this->createForm(CategoryType::class, $category);
+
+        $form->handleRequest($request);
+
+        // Was the form submitted ?
+        if ($form->isSubmitted()) {
+            $categoryRepository->save($category, true);
+
+            // Redirect to categories list
+            return $this->redirectToRoute('category_index');
+        }
+
+        // Render the form (best practice)
+        return $this->renderForm('category/new.html.twig', [
+            'form' => $form,
+        ]);
+
+        // Alternative
+        // return $this->render('category/new.html.twig', [
+        //   'form' => $form->createView(),
+        // ]);
     }
 
     #[Route('/{categoryName}', methods: ['GET'], name: 'show')]
---
+++ src/Form/ProgramType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Program;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ProgramType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title')
+            ->add('synopsis')
+            ->add('poster')
+            ->add('country')
+            ->add('year')
+            // Il va falloir modifier le FormType du champ category dans notre ProgramType
+            // en lui spécifiant la propriété de Category qui va servir de label 
+            // dans notre liste déroulante (ici name) :
+            ->add('category', null, ['choice_label' => 'name']);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Program::class,
+        ]);
+    }
+}
